2041625f06cb95adb3e00b00adbd7f4f4c35e0e9
robertcwarren1@gmail.com <bobbywarren14@gmail.com>
2022-08-29 14:19:16 -0700

    Added navbar and started adding store cards.

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,47 +1,21 @@
 import React from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.css'
 
 const Header = () => {
   return (
-    <nav className='navbar navbar-inverse navbar-static-top'>
-        <div className='navbar-header'>
-            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' 
-                data-target='#bs-example-navbar-collapse-1' aria-expanded='false'>
-                    <span className='sr-only'>Toggle Navigation</span>
-                    <span className='icon-bar'></span>
-                    <span className='icon-bar'></span>
-                    <span className='icon-bar'></span>
-            </button>
+   <div className='header-container'>
 
-            <a className='navbar-brand' href='/'>Tribute</a>
-        </div>
+      <div className='nav-links-div'>
+        <Link to='/' className='brand' style={{ textDecoration: 'none' }} >Tribute</Link>
+        <Link to='/store' className='nav-links' style={{ textDecoration: 'none' }} >Store</Link>
+        <Link to='/categories' className='nav-links' style={{ textDecoration: 'none' }} >Categories</Link>
+        <Link to='/used' className='nav-links' style={{ textDecoration: 'none' }} >Used</Link>
+        <Link to='/login' className='nav-links login' style={{ textDecoration: 'none' }} >Login</Link>
+        <Link to='/signup' className='nav-links signup' style={{ textDecoration: 'none' }} >Signup</Link>
+      </div>
 
-        <div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
-            <ul className='nav navbar-nav'>
-                <li className='active'> <a href='/'>Link <span className='sr-only'>(current)</span> </a> </li>
-
-                <li> <a href='/'>Link2</a> </li>
-
-                <li className='dropdown'>
-                    <a href='#' className='dropdown-togggle' data-toggle='dropdown' role='button' aria-haspopup='true'
-                        aria-expanded='false'>Dropdown <span className='caret'> </span> </a>
-                    
-                    <ul className='dropdown-menu'>
-                        <li><a href='/boards'>BOARDS</a> </li>
-                        <li><a href='/bindings'>BINDINGS</a> </li>
-                        <li><a href='/boots'>BOOTS</a> </li>
-                        <li><a href='/jackets'>JACKETS</a> </li>
-                        <li><a href='/pants'>PANTS</a> </li>
-                        <li><a href='/gloves'>GLOVES</a> </li>
-                        <li><a href='/goggles'>GOGGLES</a> </li>
-                        <li><a href='/beanies'>BEANIES</a> </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </nav>
+   </div>
   )
 }
 
--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Store.css'
+import Board from '../Assets/Snowboards/Hypno.png'
+import Bindings from '../Assets/Bindings/HeroRed.png'
 
 const Store = () => {
   return (
-    <div>
-      <h1>Store</h1>
+   <div className='row row-cols-1 row-cols-md-2 g-4'>
+    
+    <div className='col'>
+      <div className='card'>
+        <img src={Board} className='card-img-top' alt='Board'/>
+        <div className='card-body'>
+          <h5 className='card-title'>Boards</h5>
+          <p className='card-text'>Browse Snowboards</p>
+          <button className='boards-link' type='submit'>Browse Boards</button>
+        </div>
+      </div>
     </div>
+    
+    <div className='col'>
+      <div className='card'>
+        <img src={Bindings} className='card-img-top' alt='Bindings' />
+        <div className='card-body'>
+          <h5 className='card-title'>Bindings</h5>
+          <p className='card-text'>Browse Bindings</p>
+          <button className='boards-link' type='submit'>Browse Bindings</button>
+        </div>
+      </div>
+    </div>
+
+   </div> 
   )
 }
 
